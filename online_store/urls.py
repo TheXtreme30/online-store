f93003a3ba8831d49c0452ad1cc41c66634c543b
Sergey <thextreme.30@gmail.com>
2021-08-15 23:38:20 +0600

    add orders templates

--- online_store/urls.py
+++ online_store/urls.py
@@ -7,6 +7,7 @@ from online_store import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include(('cart.urls', 'cart'), namespace='cart')),
+    path('orders/', include('orders.urls', namespace='orders')),
     path('', include(('shop.urls', 'shop'), namespace='shop')),
 ]
 
